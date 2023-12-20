4c4b41925ec2c6c4feabc85d6867b81cfc58f6bf
Dirceu Maciel Torres <maciel.dirceu@hotmail.com>
2023-12-20 09:23:01 -0300

    aula14v3js

--- SEGUNDO_PROJETO/aula14_v2.js
+++ SEGUNDO_PROJETO/aula14_v2.js
@@ -1,13 +1,40 @@
 function verify() {
-  var anoIn = window.document.querySelector("input#ano").value;
+  var imgm = window.document.querySelector("img#imgm");
+  var dayIn = window.document.querySelector("input#dia").value;
+  var monthIn = window.document.querySelector("input#mes").value;
+  var yearIn = window.document.querySelector("input#ano").value;
   var dat = new Date();
-  var year = getFullYear();
+  var day = dat.getDate();
+  var month = dat.getMonth() + 1;
+  var year = dat.getFullYear();
   var resu = window.document.querySelector("div#d2");
-  var today = year - anoIn;
-  window.alert = today;
-  if (anoIn.value.lenght == 0 || anoIn.value > year) {
-    window.alert(" DADOS INCORRETOS! TENTE NOVAMENTE! ");
+  var dayNow = day - dayIn;
+  var monthNow = month - monthIn;
+  var yearNow = year - yearIn;
+  if (
+    dayIn <= 0 ||
+    Number(dayIn) > 31 ||
+    monthIn <= 0 ||
+    Number(monthIn) > 12 ||
+    yearIn <= 0 ||
+    Number(yearIn) > year
+  ) {
+    imgm.src = "erro.png";
   } else {
-    window.alert("OK!");
+    resu.innerHTML = `VOCÊ TEM ${yearNow} ANOS, ${monthNow} MESES E ${dayNow} DIAS DE VIDA!`;
   }
+}
+function clean() {
+  window.document.querySelector("input#dia").value = "";
+  window.document.querySelector("input#mes").value = "";
+  window.document.querySelector("input#ano").value = "";
+  window.document.querySelector("div#d2").textContent = "";
+  window.document.querySelector("img#imgm").src = "";
+}
+function popup(imageSrc) {
+  var popup = window.document.querySelector("img#imgm");
+  imgm.src = imageSrc;
+}
+function closepop() {
+  window.document.querySelector("div#d3").style.display = "none";
 }
